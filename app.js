a4d0751b37a5cad6768cfe07fc4ac07ac3c9c4a7
jannatfaisal <118313474+jannatfaisal@users.noreply.github.com>
2024-08-11 23:14:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// chp3
+
+// Q1
+
+// var age=20;
+//  alert("I am " + age + " years old");
+
+
+
+// Q2
+//  var numOfTime=15;
+
+//  alert( "you have visited this site " + numOfTime);
+
+
+// Q3
+// var birthYear = 2003;
+
+// document.write("my birth year is "  + birthYear);
+
+// Q4
+
+// var vistor_Name="john";
+
+// var Quantity="5 T-shirt(s)"
+// var product_Title="on XYZ Clothing store"
+
+// document.write(vistor_Name + " "+ Quantity+ " " +product_Title);
+
